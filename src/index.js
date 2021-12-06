acb47161b5817399a843425431ea917c158523f9
Suyog-23 <suyog231002@gmail.com>
2021-12-06 12:58:17 +0530

    Chakra-UI added

--- src/index.js
+++ src/index.js
@@ -3,13 +3,23 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ChakraProvider } from '@chakra-ui/react'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function tick(){
+
+  ReactDOM.render(
+    <React.StrictMode>
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+  
+
+}
+
+setInterval(tick, 1000)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
